f88ee2a5170f47aa7e8f75c75530ff1313e28662
Nicholas Swan <nicholasgswan@gmail.com>
2026-01-22 00:26:43 -0800

    seperating files as instructed

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		b := make([]byte, 8)
+		//var offset int64 = 0
+		var s strings.Builder
+		var err error
+		for err == nil {
+			//var l int
+			_, err = f.Read(b)
+			arr := strings.Split(string(b), "\n")
+			s.WriteString(arr[0])
+			if len(arr) > 1 {
+				out <- s.String()
+				s.Reset()
+				s.WriteString(arr[1])
+			}
+			if err == io.EOF {
+				out <- s.String()
+			}
+
+			clear(b)
+			//offset += 8
+		}
+
+	}()
+
+	return out
+}
+
+func main() {
+	//f, err := os.Open("messages.txt")
+
+	lis, err := net.Listen("tcp", "127.0.0.1:42069")
+	defer lis.Close()
+	if err != nil {
+		fmt.Println("oops")
+		return
+	}
+
+	for true {
+		conn, err1 := lis.Accept()
+		if err1 != nil {
+			fmt.Println("oops")
+			return
+		}
+		fmt.Println("Connection has been accepted!")
+		lines := getLinesChannel(conn)
+		for line := range lines {
+			fmt.Println(line)
+		}
+		fmt.Println("Connection is closed")
+	}
+
+}
