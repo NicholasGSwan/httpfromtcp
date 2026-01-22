bc45d006c14d7f6be76bbee505bd894c438f0308
Nicholas Swan <nicholasgswan@gmail.com>
2026-01-22 14:34:08 -0800

    did initial request line parse and tests

---
+++ internal/request/request.go
@@ -0,0 +1,57 @@
+package request
+
+import (
+	"errors"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	req := Request{}
+	b := make([]byte, 8)
+
+	var s strings.Builder
+	var err error
+	for err == nil {
+		_, err = reader.Read(b)
+
+		arr := strings.Split(string(b), "\r\n")
+		s.WriteString(arr[0])
+		if len(arr) > 1 {
+			break
+		}
+
+	}
+
+	rl, err := parseRequestLine(s.String())
+
+	req.RequestLine = *rl
+
+	return &req, err
+}
+
+func parseRequestLine(req string) (*RequestLine, error) {
+	rl := RequestLine{}
+
+	arr := strings.Split(req, " ")
+
+	if len(arr) != 3 {
+		return &rl, errors.New("Invalid Request Line")
+	}
+
+	rl.Method = arr[0]
+	rl.RequestTarget = arr[1]
+	rl.HttpVersion = strings.Split(arr[2], "/")[1]
+
+	return &rl, nil
+}
