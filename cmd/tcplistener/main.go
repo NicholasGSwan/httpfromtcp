aaeb5fab12af2b608ad47b39366627eeaad89533
Nicholas Swan <nicholasgswan@gmail.com>
2026-01-23 23:30:10 -0800

    update tcplistener to use request reader

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,43 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"log"
 	"net"
-	"strings"
+
+	"github.com/nicholasgswan/httpfromtcp/internal/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	out := make(chan string, 1)
 
-	go func() {
-		defer f.Close()
-		defer close(out)
-		b := make([]byte, 8)
-		//var offset int64 = 0
-		var s strings.Builder
-		var err error
-		for err == nil {
-			//var l int
-			_, err = f.Read(b)
-			arr := strings.Split(string(b), "\n")
-			s.WriteString(arr[0])
-			if len(arr) > 1 {
-				out <- s.String()
-				s.Reset()
-				s.WriteString(arr[1])
-			}
-			if err == io.EOF {
-				out <- s.String()
-			}
+// 	go func() {
+// 		defer f.Close()
+// 		defer close(out)
+// 		b := make([]byte, 8)
+// 		//var offset int64 = 0
+// 		var s strings.Builder
+// 		var err error
+// 		for err == nil {
+// 			//var l int
+// 			_, err = f.Read(b)
+// 			arr := strings.Split(string(b), "\n")
+// 			s.WriteString(arr[0])
+// 			if len(arr) > 1 {
+// 				out <- s.String()
+// 				s.Reset()
+// 				s.WriteString(arr[1])
+// 			}
+// 			if err == io.EOF {
+// 				out <- s.String()
+// 			}
 
-			clear(b)
-			//offset += 8
-		}
+// 			clear(b)
+// 			//offset += 8
+// 		}
 
-	}()
+// 	}()
 
-	return out
-}
+// 	return out
+// }
 
 func main() {
 	//f, err := os.Open("messages.txt")
@@ -46,8 +47,7 @@ func main() {
 	lis, err := net.Listen("tcp", "127.0.0.1:42069")
 
 	if err != nil {
-		fmt.Println("oops")
-		return
+		log.Fatalf("oops... %s\n", err.Error())
 	}
 	defer lis.Close()
 	for true {
@@ -57,10 +57,12 @@ func main() {
 			return
 		}
 		fmt.Println("Connection has been accepted!")
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("invalid request: %s\n", err.Error())
 		}
+		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion)
+
 		fmt.Println("Connection is closed")
 	}
 
