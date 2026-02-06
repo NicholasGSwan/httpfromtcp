6d5eb6df5e0a2314adad3afe01a1eada9afa6970
Nicholas Swan <NicholasGSwan@gmail.com>
2026-02-06 12:00:04 -0500

    commiting work so far

--- internal/response/response.go
+++ internal/response/response.go
@@ -1,1 +1,55 @@
 package response
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/nicholasgswan/httpfromtcp/internal/headers"
+)
+
+type StatusCode int
+
+const (
+	OK          StatusCode = 200
+	BadRequest  StatusCode = 400
+	ServerError StatusCode = 500
+)
+
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	var err error
+	switch statusCode {
+	case OK:
+		_, err = w.Write([]byte("HTTP/1.1 200 OK\r\n"))
+	case BadRequest:
+		_, err = w.Write([]byte("HTTP/1.1 400 Bad Request\r\n"))
+	case ServerError:
+		_, err = w.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n"))
+	default:
+		fmt.Println("Did not write statuscode")
+	}
+	return err
+}
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	h := headers.Headers{}
+	h.Set("content-length", strconv.Itoa(contentLen))
+	h.Set("connection", "close")
+	h.Set("content-type", "text/plain")
+	return h
+
+}
+
+func WriteHeaders(w io.Writer, headers headers.Headers) error {
+	for k, v := range headers {
+		_, err := fmt.Fprintf(w, "%s: %s\r\n", k, v)
+		if err != nil {
+			return err
+		}
+	}
+	_, err := w.Write([]byte("\r\n"))
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,35 +3,45 @@ package server
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"sync/atomic"
+
+	"github.com/nicholasgswan/httpfromtcp/internal/request"
+	"github.com/nicholasgswan/httpfromtcp/internal/response"
 )
 
-const (
-	response string = "HTTP/1.1 200 OK\r\n" +
-		"Content-Type: text/plain\r\n" +
-		//	"Content-Length: 13\r\n\r\n" +
-		"\r\n" +
-		"Hello World!\n"
-)
+// const (
+// 	response string = "HTTP/1.1 200 OK\r\n" +
+// 		"Content-Type: text/plain\r\n" +
+// 		//	"Content-Length: 13\r\n\r\n" +
+// 		"\r\n" +
+// 		"Hello World!\n"
+// )
 
 type Server struct {
 	Open atomic.Bool
 	port int
 }
 
+type HandlerError struct {
+	statusCode response.StatusCode
+	msg        string
+}
+
 func Serve(port int) (*Server, error) {
-	s := Server{Open: atomic.Bool{}, port: port}
+	s := &Server{Open: atomic.Bool{}, port: port}
+
 	s.Open.Store(true)
-	s.listen()
-	return &s, nil
+	go s.listen()
+	return s, nil
 }
 
 func (s *Server) Close() error {
-	closed := s.Open.Swap(false)
-	if !closed {
+	s.Open.Store(false)
+	if s.Open.Load() {
 		return errors.New("Could not close server!")
 	}
 	return nil
@@ -51,16 +61,22 @@ func (s *Server) listen() {
 			return
 		}
 		fmt.Println("Connection Open")
-		//defer conn.Close()
-		go func() {
 
-			s.handle(conn)
-		}()
+		go s.handle(conn)
+
 	}
 
 }
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
 func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
 	fmt.Println("sending response")
-	conn.Write([]byte(response))
+	h := response.GetDefaultHeaders(0)
+	response.WriteStatusLine(conn, response.OK)
+	if err := response.WriteHeaders(conn, h); err != nil {
+		fmt.Printf("error: %v/n", err)
+	}
+	return
 }
